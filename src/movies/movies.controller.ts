52ab275a611a186e8600af04692205e180cd0a7e
mjpark <gi6238@naver.com>
2020-11-30 20:23:14 +0900

    movies routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,15 +15,18 @@ export class MoviesController {
   getAll() {
     return 'This will return all movies';
   }
-
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -24,7 +36,11 @@ export class MoviesController {
 
   //전체 업데이트시, @Put, 일부분만 업데이트시 @Patch
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
     return `This will patch a movie with the id:${movieId}`;
   }
 }
