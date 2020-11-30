f70d0f18ac03fbabbecc85caa2c9e6ce181a1d06
mjpark <gi6238@naver.com>
2020-11-30 21:33:49 +0900

    DTOs and validators

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,8 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -23,23 +25,23 @@ export class MoviesController {
     return `We are searching for a movie made after: ${searchingYear}`;
   }
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesServices.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesServices.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesServices.deleteOne(movieId);
   }
 
   //전체 업데이트시, @Put, 일부분만 업데이트시 @Patch
   @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesServices.update(movieId, updateData);
   }
 }
